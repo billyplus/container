c9472efcc31f1edbd1e29ec51a12fbecbd196a8c
billy <me@ybilly.com>
2025-05-16 23:04:15 +0800

    more tests

--- timewheel/wheel_test.go
+++ timewheel/wheel_test.go
@@ -9,13 +9,14 @@ import (
 )
 
 func TestTimeWheelMs(t *testing.T) {
-	mslist := []int64{172223430, 172223436, 172223440, 172223441, 172223439, 1747312309251, time.Now().UnixMilli()}
+	mslist := []int64{172223430, 172223436, 172223440, 172223441, 172223439, 1747312310251, time.Now().UnixMilli()}
 	// mslist := []int64{1747321355416}
 	// wheel.hand = mili
 	// wheel.Update(nowMs)
 	// nowMs += 32 * 60 * 1000
 	// wheel.Update(nowMs)
 	delaymap := map[int64]int{
+		3:             2,
 		29:            2,
 		49:            2,
 		50:            3,
@@ -24,7 +25,9 @@ func TestTimeWheelMs(t *testing.T) {
 		200:           2,
 		300:           1,
 		1100:          1,
+		3223:          64,
 		4210:          2,
+		59010:         2,
 		5 * 60 * 1000: 2,
 	}
 
@@ -66,4 +69,39 @@ func addTestCase(wheel *TimeWheelMilliSecond, t *testing.T, startTime, delay int
 		assert.GreaterOrEqualf(t, ms-tickTime, int64(0), "[%d+%d]间隔大于0", startTime, delay)
 		(*result)[delay] = (*result)[delay] + 1
 	})
+}
+
+func TestTimeWheelMsExpand(t *testing.T) {
+	mslist := []int64{time.Now().UnixMilli()}
+	// mslist := []int64{1747321355416}
+	// wheel.hand = mili
+	// wheel.Update(nowMs)
+	// nowMs += 32 * 60 * 1000
+	// wheel.Update(nowMs)
+	delaymap := map[int64]int{
+		200: 1030,
+	}
+
+	for _, ms := range mslist {
+		result := make(map[int64]int)
+		wheel := NewTimeWheelMilliSecond()
+		maxDelay := int64(0)
+		for delay, count := range delaymap {
+			if maxDelay < delay {
+				maxDelay = delay
+			}
+			for range count {
+				addTestCase(wheel, t, ms, delay, &result)
+			}
+		}
+		nowMs := ms
+		wheel.Update(nowMs)
+		for range maxDelay/3 + 5 {
+			nowMs += 3
+			wheel.Update(nowMs)
+		}
+		for delay, count := range delaymap {
+			assert.Equalf(t, count, result[delay], "ms %d delay %d should excute %d times", ms, delay, count)
+		}
+	}
 }
